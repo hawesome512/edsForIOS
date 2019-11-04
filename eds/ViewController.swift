55cc5d217c0e5cdb56459788a51d40364160c674
Hawesome <haisheng.xu@xseec>
2019-11-04 10:26:12 +0800

    在Xcode中做git管理测试

---
+++ eds/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  eds
+//
+//  Created by 厦门士林电机有限公司 on 2019/11/4.
+//  Copyright © 2019 厦门士林电机有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let stringj = "a"
+    }
+
+
+}
+
